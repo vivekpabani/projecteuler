fab21c71a052d0d370a36cffe00a757563568065
vivekpabani <viky.pabani@gmail.com>
2014-12-14 18:52:14 -0600

    First commit

---
+++ python/22/problem_022.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+problem definition:
+
+Using names.txt, a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+
+"""
+
+__author__ = 'vivek'
+
+import string
+
+# dictionary with character values of uppercase alphabets.
+alpha_dict = dict((ch, ord(ch)-64) for ch in string.ascii_uppercase)
+
+with open("names.txt") as f:
+    words = f.read()
+
+# replace the quotes and store into a list with coma separator and sort list.
+words = string.replace(words,'"','')
+word_list = words.split(',')
+word_list.sort()
+
+# find value of each word, and add to the final total with count multiplication.
+file_total = 0
+count = 0
+for word in word_list:
+    count = count+1
+    word_total = 0
+    for letter in word:
+        word_total = word_total + alpha_dict[letter]
+    file_total = file_total+ (count*word_total)
+
+print "File Total = ", file_total
