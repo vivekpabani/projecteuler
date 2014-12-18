0f92aa2d960505cc34f383af392f92253d415d6e
vivekpabani <viky.pabani@gmail.com>
2014-12-17 21:34:36 -0600

    update

--- python/038/temp.py
+++ python/038/temp.py
@@ -4,31 +4,5 @@ import time
 import math
 
 startTime = time.clock()
-"""
-#one_d = [i for i in xrange(1,10)]
-one_d = ['98','87','642']
-#one_d = [9,87,642]
-print(one_d)
-one_d.sort()
-print(one_d)
-one_d.sort(reverse=True)
-print(one_d)
-print ''.join(str(i) for i in one_d)
 
-
-name = "abcde"
-while name:
-    name = name[1:]
-    print(name)
-
-name = "abcde"
-while name:
-    name = name[:-1]
-    print(name)
-#print(name[1:])
-#print(name[:-1])
-"""
-print divmod(152,10)
-print(15.0/7)
-print()
 print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
