53b69db43de316e3a05ff9e9a24830b4733cb597
vivekpabani <viky.pabani@gmail.com>
2014-12-24 10:26:27 -0600

    First Update

---
+++ python/056/problem_056.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+"""
+Problem Definition :
+
+A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+"""
+
+__author__ = 'vivek'
+
+import time
+
+startTime = time.clock()
+
+#print(pow(99,99))
+maximum = 0
+
+for a in xrange(95,100):
+    for b in xrange(95,100):
+        answer = pow(a,b)
+        total = 0
+        while(answer>0):
+            answer, digit = divmod(answer,10)
+            total += digit
+        maximum = max(maximum,total)
+print(maximum)
+print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
