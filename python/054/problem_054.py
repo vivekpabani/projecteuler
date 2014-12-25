9a600c8f10ba76d7e7d18df592a44e52dbbf089e
vivekpabani <viky.pabani@gmail.com>
2014-12-25 11:06:27 -0600

    Update

---
+++ python/054/problem_054.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+"""
+Problem Definition :
+
+
+"""
+
+__author__ = 'vivek'
+
+import time
+
+startTime = time.clock()
+
+print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
