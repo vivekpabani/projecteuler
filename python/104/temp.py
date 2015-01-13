2d6feac54a68aec88e1acb7b3e0557e1402360c0
vivekpabani <viky.pabani@gmail.com>
2015-01-13 08:44:48 -0600

    Update

---
+++ python/104/temp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+"""
+Problem Definition :
+
+
+"""
+
+__author__ = 'vivek'
+
+import time
+
+startTime = time.clock()
+
+name = 'abcde'
+
+print(name[:3])
+
+print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
