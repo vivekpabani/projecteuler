0bd7837376f392c943f0ecc637b11baca3542429
vivekpabani <viky.pabani@gmail.com>
2015-01-14 09:09:38 -0600

    First Update

---
+++ python/125/problem_125.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+"""
+Problem Definition :
+
+The palindromic number 595 is interesting because it can be written as the sum of consecutive squares: 62 + 72 + 82 + 92 + 102 + 112 + 122.
+
+There are exactly eleven palindromes below one-thousand that can be written as consecutive square sums, and the sum of these palindromes is 4164. Note that 1 = 02 + 12 has not been included as this problem is concerned with the squares of positive integers.
+
+Find the sum of all the numbers less than 108 that are both palindromic and can be written as the sum of consecutive squares.
+
+"""
+
+__author__ = 'vivek'
+
+import time
+import math
+
+startTime = time.clock()
+maximum = 100000001
+limit = int(math.sqrt(maximum))+1
+
+matrix = [[0 for x in xrange(limit)] for x in xrange(limit)]
+answers = []
+
+for i in xrange(1,limit):
+    for j in xrange(i, limit):
+        matrix[i][j] =  matrix[i][j-1] + (j*j)
+        if str(matrix[i][j])==str(matrix[i][j])[::-1] and matrix[i][j] < maximum and j!= i:
+            answers.append(matrix[i][j])
+
+print(sum(set(answers)))
+
+print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
+
