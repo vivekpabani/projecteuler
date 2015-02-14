85684966b3cb590ee42d414229f294cf14585b12
vivekpabani <viky.pabani@gmail.com>
2015-02-14 10:02:58 -0600

    fun update

--- python/030/problem_030.py
+++ python/030/problem_030.py
@@ -24,21 +24,24 @@ import time
 import math
 
 
-startTime = time.clock()
-matched_numbers = []
+def main():
 
-for count in xrange(2,1000000):
-    number = count
-    addition = 0
-    while(number>0):
-        digit = number%10
-        number = number/10
-        addition = addition + math.pow(digit,5)
-    if addition == count:
-        matched_numbers.append(count)
+    start_time = time.clock()
+    matched_numbers = []
 
-print(matched_numbers)
+    for count in xrange(2, 1000000):
+        number = count
+        addition = 0
+        while number > 0:
+            digit = number % 10
+            number /= 10
+            addition += math.pow(digit, 5)
+        if addition == count:
+            matched_numbers.append(count)
 
-print sum(matched_numbers)
+    print sum(matched_numbers)
 
-print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
+    print "Run time...{} secs \n".format(round(time.clock() - start_time, 4))
+
+if __name__ == '__main__':
+    main()
