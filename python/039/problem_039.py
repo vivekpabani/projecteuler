d8121bf4eb98b7266eaff5b29473e6bc554b43a1
vivekpabani <viky.pabani@gmail.com>
2015-02-22 14:54:47 -0600

    Def update

--- python/039/problem_039.py
+++ python/039/problem_039.py
@@ -4,7 +4,8 @@
 """
 Problem Definition :
 
-If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions
+for p = 120.
 
 {20,48,52}, {24,45,51}, {30,40,50}
 
@@ -15,31 +16,35 @@ __author__ = 'vivek'
 
 import time
 
-startTime = time.clock()
 
-square_l = []
-square_d = {}
-answers = [0]*1001
-limit = 1000
-maximum = limit/2
-for number in xrange(1,500):
-    square = number**2
-    square_l.append(square)
-    square_d[square] = number
+def main():
+    start_time = time.clock()
+
+    square_l = []
+    square_d = {}
+    answers = [0]*1001
+    limit = 1000
+    maximum = limit/2
+
+    for number in xrange(1, 500):
+        square = number**2
+        square_l.append(square)
+        square_d[square] = number
+
+    for number1 in xrange(3, maximum):
+        for number2 in xrange(1, maximum-number1):
+            if number2 > number1:
+                break
+            add = number1**2 + number2**2
+            if add in square_l:
+                answer = number1 + number2 + square_d[add]
+                if answer < 1001:
+                    answers[answer] += 1
+
+    print(answers.index(max(answers)))
+
+    print "Run time...{} secs \n".format(round(time.clock() - start_time, 4))
 
 
-
-for number1 in xrange(3,maximum):
-    for number2 in xrange(1,maximum-number1):
-        if number2>number1:
-            break
-        add = number1**2 + number2**2
-        if add in square_l :
-            answer = number1+number2+square_d[add]
-            if answer < 1001:
-                answers[answer] += 1
-
-print(answers.index(max(answers)))
-
-
-print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
+if __name__ == '__main__':
+    main()
