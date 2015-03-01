a00e0d2e794a6259ae0f44caf6931fabf78c320a
vivekpabani <viky.pabani@gmail.com>
2015-03-01 10:42:02 -0600

    Def Update

--- python/050/problem_050.py
+++ python/050/problem_050.py
@@ -12,10 +12,6 @@ __author__ = 'vivek'
 import time
 import math
 
-startTime = time.clock()
-
-prime_numbers = []
-
 
 def is_prime(number):
     if number < 0:
@@ -37,38 +33,45 @@ def is_prime(number):
         return 1
 
 
-for x in xrange(1,3944):
-    if is_prime(x):
-        prime_numbers.append(x)
+def main():
 
+    start_time = time.clock()
 
-length = len(prime_numbers)
+    prime_numbers = []
 
-matrix = [[0 for x in range(length)] for x in range(length)]
+    for x in xrange(1, 3944):
+        if is_prime(x):
+            prime_numbers.append(x)
 
+    length = len(prime_numbers)
 
-for j in xrange(0,length):
-    try:
-        matrix[0][j] = matrix[0][j-1] + prime_numbers[j]
-    except:
-        matrix[0][j] = prime_numbers[j]
+    matrix = [[0 for x in range(length)] for x in range(length)]
 
+    for j in xrange(0, length):
+        try:
+            matrix[0][j] = matrix[0][j-1] + prime_numbers[j]
+        except:
+            matrix[0][j] = prime_numbers[j]
 
-for i in xrange(1,length):
-    for j in xrange(i,length):
-        matrix[i][j] = matrix[i-1][j] - matrix[i-1][i-1]
+    for i in xrange(1, length):
+        for j in xrange(i, length):
+            matrix[i][j] = matrix[i-1][j] - matrix[i-1][i-1]
 
+    found = 0
 
-found = 0
-
-for num1 in xrange(0,length):
-    for num2 in xrange(0,num1):
-        if is_prime(matrix[num2][length-num1+num2]):
-            print(num2,length-num1+num2)
-            print(matrix[num2][length-num1+num2])
-            found = 1
+    for num1 in xrange(0, length):
+        for num2 in xrange(0, num1):
+            if is_prime(matrix[num2][length-num1+num2]):
+                print(num2, length-num1+num2)
+                print(matrix[num2][length-num1+num2])
+                found = 1
+                break
+        if found:
             break
-    if found:
-        break
 
-print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
+    print "Run time...{} secs \n".format(round(time.clock() - start_time, 4))
+
+
+if __name__ == '__main__':
+    main()
+
