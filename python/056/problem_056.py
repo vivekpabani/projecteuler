d0faedd11f1b8face6cbb956daec657651d51cd2
vivekpabani <viky.pabani@gmail.com>
2015-03-04 10:28:19 -0600

    Def Update

--- python/056/problem_056.py
+++ python/056/problem_056.py
@@ -4,7 +4,8 @@
 """
 Problem Definition :
 
-A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one
+followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
 
 Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
 """
@@ -13,23 +14,30 @@ __author__ = 'vivek'
 
 import time
 
-startTime = time.clock()
 
-maximum = 0
+def main():
 
-for a in xrange(95,100):
-    for b in xrange(95,100):
+    start_time = time.clock()
 
-        answer = pow(a,b)
-        total = 0
+    maximum = 0
 
-        while answer > 0:
+    for a in xrange(95, 100):
+        for b in xrange(95, 100):
 
-            answer, digit = divmod(answer,10)
-            total += digit
+            answer = pow(a, b)
+            total = 0
 
-        maximum = max(maximum,total)
+            while answer > 0:
 
-print(maximum)
+                answer, digit = divmod(answer, 10)
+                total += digit
 
-print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
+            maximum = max(maximum, total)
+
+    print(maximum)
+
+    print "Run time...{} secs \n".format(round(time.clock() - start_time, 4))
+
+
+if __name__ == '__main__':
+    main()
