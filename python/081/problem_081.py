bd7b0d089fc249a24da451dbc9a5d3122b2e3afc
vivekpabani <viky.pabani@gmail.com>
2015-03-14 10:18:35 -0500

    Def Update

--- python/081/problem_081.py
+++ python/081/problem_081.py
@@ -13,39 +13,46 @@ __author__ = 'vivek'
 
 import time
 
-startTime = time.clock()
 
-value_matrix = []
-for line in open('matrix.txt'):
+def main():
 
-    separator = ','
-    temp_list = []
+    start_time = time.clock()
 
-    line = line.split(separator)
+    value_matrix = list()
 
-    for value in line:
-        temp_list.append(int(value))
-    value_matrix.append(temp_list)
+    for line in open('matrix.txt'):
 
+        separator = ','
+        temp_list = list()
 
-length = len(value_matrix)
+        line = line.split(separator)
 
-answer_matrix = [[0]*length for i in range(length)]
+        for value in line:
+            temp_list.append(int(value))
+        value_matrix.append(temp_list)
 
-for i in xrange(0, length):
-    for j in xrange(0, length):
-        if i == 0 and j == 0:
-            answer_matrix[i][j] = value_matrix[i][j]
-        elif i == 0 and j > 0:
-            answer_matrix[i][j] = value_matrix[0][j] + answer_matrix[0][j-1]
-        elif i > 0 and j == 0:
-            answer_matrix[i][j] = value_matrix[i][0] + answer_matrix[i-1][0]
-        else:
-            if answer_matrix[i-1][j]<answer_matrix[i][j-1]:
-                answer_matrix[i][j] = value_matrix[i][j] + answer_matrix[i-1][j]
+    length = len(value_matrix)
+
+    answer_matrix = [[0]*length for i in range(length)]
+
+    for i in xrange(0, length):
+        for j in xrange(0, length):
+            if i == 0 and j == 0:
+                answer_matrix[i][j] = value_matrix[i][j]
+            elif i == 0 and j > 0:
+                answer_matrix[i][j] = value_matrix[0][j] + answer_matrix[0][j-1]
+            elif i > 0 and j == 0:
+                answer_matrix[i][j] = value_matrix[i][0] + answer_matrix[i-1][0]
             else:
-                answer_matrix[i][j] = value_matrix[i][j] + answer_matrix[i][j-1]
+                if answer_matrix[i-1][j] < answer_matrix[i][j-1]:
+                    answer_matrix[i][j] = value_matrix[i][j] + answer_matrix[i-1][j]
+                else:
+                    answer_matrix[i][j] = value_matrix[i][j] + answer_matrix[i][j-1]
 
-print answer_matrix[length-1][length-1]
+    print answer_matrix[length-1][length-1]
 
-print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
+    print "Run time...{} secs \n".format(round(time.clock() - start_time, 4))
+
+
+if __name__ == '__main__':
+    main()
