82fe8a15cce42d7d453d4e781553d41334963bcf
vivekpabani <viky.pabani@gmail.com>
2015-03-15 10:15:19 -0500

    Def Update

--- python/087/problem_087.py
+++ python/087/problem_087.py
@@ -20,67 +20,72 @@ __author__ = 'vivek'
 import time
 import math
 
-startTime = time.clock()
 
-
-def is_prime(number):
-    if number < 0:
+def is_prime(num):
+    if num < 0:
         return 0
-    elif number == 2 or number == 3:
+    elif num == 2 or num == 3:
         return 1
-    elif number % 2 == 0 or number % 3 == 0 or number == 1:
+    elif num % 2 == 0 or num % 3 == 0 or num == 1:
         return 0
     else:
         start = 5
-        while start <= int(math.sqrt(number)):
-            if number % start == 0:
+        while start <= int(math.sqrt(num)):
+            if num % start == 0:
                 return 0
                 break
-            if number % (start+2) == 0:
+            if num % (start+2) == 0:
                 return 0
                 break
             start += 6
         return 1
 
 
-prime_numbers = [x for x in xrange(1,8000) if is_prime(x)]
+def main():
+    start_time = time.clock()
+
+    prime_numbers = [x for x in xrange(1, 8000) if is_prime(x)]
+
+    p2 = list()
+    p3 = list()
+    p4 = list()
+
+    answers = list()
+
+    for number in prime_numbers:
+        num1 = number**2
+        if num1 < 50000000:
+            p2.append(num1)
+        else:
+            break
+
+    for number in prime_numbers:
+        num2 = number**3
+        if num2 < 50000000:
+            p3.append(num2)
+        else:
+            break
+
+    for number in prime_numbers:
+        num3 = number**4
+        if num3 < 50000000:
+            p4.append(num3)
+        else:
+            break
+
+    for num1 in p2:
+        for num2 in p3:
+            for num3 in p4:
+                answer = num1 + num2 + num3
+                if answer < 50000000:
+                    answers.append(answer)
+    answers = list(set(answers))
+
+    print(len(answers))
+
+    print "Run time...{} secs \n".format(round(time.clock() - start_time, 4))
 
 
-p2 = []
-p3 = []
-p4 = []
+if __name__ == '__main__':
+    main()
 
-answers = []
-
-for number in prime_numbers:
-    num = number**2
-    if num < 50000000:
-        p2.append(num)
-    else:
-        break
-
-for number in prime_numbers:
-    num = number**3
-    if num < 50000000:
-        p3.append(num)
-    else:
-        break
-
-for number in prime_numbers:
-    num = number**4
-    if num < 50000000:
-        p4.append(num)
-    else:
-        break
-
-for num1 in p2:
-    for num2 in p3:
-        for num3 in p4:
-            answer = num1 + num2 + num3
-            if answer < 50000000:
-                answers.append(answer)
-answers = list(set(answers))
-
-print(len(answers))
-
-print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
