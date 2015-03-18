ae2d782624f4850b0d81445e1c7cd7e4cc982456
vivekpabani <viky.pabani@gmail.com>
2015-03-18 11:30:55 -0500

    Def Update

--- python/004/problem_004.py
+++ python/004/problem_004.py
@@ -8,17 +8,24 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
-num = 998001
-flag = 1
 
-while num > 10000 and flag :
-    x = 999
-    if str(num) == str(num)[::-1] :
-        while x > 99 and flag :
-            if num%x == 0 and len(str(num/x)) == 3 :
-                print "Num1 : ", x
-                print "Num2 : ", num/x
-                print "Palindrome : ", num 
-                flag = 0
-            x = x-1
-    num = num-1
+def main():
+
+    num = 998001
+    flag = 1
+
+    while num > 10000 and flag:
+        x = 999
+        if str(num) == str(num)[::-1]:
+            while x > 99 and flag:
+                if num % x == 0 and len(str(num/x)) == 3:
+                    print "Num1 : ", x
+                    print "Num2 : ", num/x
+                    print "Palindrome : ", num
+                    flag = 0
+                x -= 1
+        num -= 1
+
+
+if __name__ == '__main__':
+    main()
