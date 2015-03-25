f0ea40b20e4fde5a7d16c1ac03d26d677b7d6607
vivekpabani <viky.pabani@gmail.com>
2015-03-25 10:20:02 -0500

    Def Update

--- python/003/problem_003.py
+++ python/003/problem_003.py
@@ -10,14 +10,17 @@ The prime factors of 13195 are 5, 7, 13 and 29.What is the largest prime factor 
 import math
 
 
-num = 600851475143
+def main():
+    num = 600851475143
+    i = 2
 
-i = 2
+    while i <= int(math.sqrt(num)):     # search up to square root of number for the max prime
+        while num % i == 0:
+            num /= i                    # divide the number by
+        i += 1
+        
+    print num
 
-while(i<=int(math.sqrt(num))) : #search upto square root of number for the max prime
-    while (num%i==0) :
-        num = num/i             #divide the number by 
-    i = i+1
-        
-print num
-        
+
+if __name__ == '__main__':
+    main()
