58da717f47f15d13ef9f4d2a75a73155edcb16fd
vivekpabani <viky.pabani@gmail.com>
2015-03-27 09:31:21 -0500

    Def Update

--- python/009/problem_009.py
+++ python/009/problem_009.py
@@ -13,11 +13,20 @@ Find the product abc.
 import math
 import sys
 
-for a in xrange(1,1001) :
-    for b in xrange (a,1001) :
-        c = a**2 + b**2
-        d=math.sqrt(c)
-        if d.is_integer() and a+b+d == 1000:
-            print a, b, int(d) 
-            sys.exit(0)
+
+def main():
+
+    for a in xrange(1, 1001):
+        for b in xrange (a, 1001):
+            c = a**2 + b**2
+            d = math.sqrt(c)
+
+            if d.is_integer() and a+b+d == 1000:
+                print a, b, int(d)
+                sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
+
         
