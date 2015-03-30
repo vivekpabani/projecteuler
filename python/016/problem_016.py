401c9f8e6cd1f7bed01f67fdf38f3f3c5f9c6648
vivekpabani <viky.pabani@gmail.com>
2015-03-30 00:13:52 -0500

    Def Update

--- python/016/problem_016.py
+++ python/016/problem_016.py
@@ -14,16 +14,22 @@ __author__ = 'vivek'
 
 import time
 
-startTime = time.clock()
 
-number = pow(2,1000)
-add = 0
+def main():
+    start_time = time.clock()
 
-while number > 0 :
-    digit = number % 10
-    number = number/10
-    add = add + digit
+    number = pow(2, 1000)
+    add = 0
 
-print(add)
+    while number > 0 :
+        digit = number % 10
+        number /= 10
+        add += digit
 
-print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
+    print(add)
+
+    print "Run time...{} secs \n".format(round(time.clock() - start_time, 4))
+
+
+if __name__ == '__main__':
+    main()
