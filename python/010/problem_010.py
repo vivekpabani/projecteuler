0acedcbc27df52992501766b50c411b6a380fbb9
vivekpabani <viky.pabani@gmail.com>
2015-03-31 11:09:12 -0500

    Def Update

--- python/010/problem_010.py
+++ python/010/problem_010.py
@@ -11,29 +11,36 @@ Find the sum of all the primes below two million.
 import math
 
 
-def isPrime(number) :
-    if number == 2 or number == 3 :
+def is_prime(number):
+    if number == 2 or number == 3:
         return 1
-    elif number % 2 == 0 or number % 3 ==0 or number == 1 :
+    elif number % 2 == 0 or number % 3 == 0 or number == 1:
         return 0
-    else :
+    else:
         start = 5   
-        while (start <= int(math.sqrt(number))) :
+        while start <= int(math.sqrt(number)):
             
-            if(number % start == 0) :
+            if number % start == 0:
                 return 0
                 break
-            if(number % (start+2) == 0) :
+            if number % (start+2) == 0:
                 return 0
                 break
-            start = start + 6
+            start += 6
         return 1 
 
-answer, num = 2,3
 
-while num < 2000001:
-    if isPrime(num):
-        answer += num
-    num += 2
+def main():
 
-print answer
+    answer, num = 2, 3
+
+    while num < 2000001:
+        if is_prime(num):
+            answer += num
+        num += 2
+
+    print answer
+
+
+if __name__ == '__main__':
+    main()
