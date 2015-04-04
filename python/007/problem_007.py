0ca85d0ac7fed902350be27602a500fce8d0ba17
vivekpabani <viky.pabani@gmail.com>
2015-04-04 10:36:19 -0500

    Def Update

--- python/007/problem_007.py
+++ python/007/problem_007.py
@@ -10,31 +10,36 @@ What is the 10001st prime number?
 
 import math
 
-def isPrime(number) :
-    if number == 2 or number == 3 :
+
+def is_prime(number):
+    if number == 2 or number == 3:
         return 1
-    elif number % 2 == 0 or number % 3 ==0 or number == 1 :
+    elif number % 2 == 0 or number % 3 == 0 or number == 1:
         return 0
-    else :
-        start = 5   
-        while (start <= int(math.sqrt(number))) :
-        
-            if(number % start == 0) :
-               return 0
-               break
-            if(number % (start+2) == 0) :
-               return 0
-               break
-            start = start + 6
+    else:
+        start = 5
+        while start <= int(math.sqrt(number)):
+
+            if number % start == 0:
+                return 0
+                break
+            if number % (start+2) == 0:
+                return 0
+                break
+            start += 6
         return 1 
 
 
-num = 1
-count = 1
+def main():
+    num = 1
+    count = 1
 
-while count < 10001 :
-    num = num + 2
-    if isPrime(num) :
-        count = count + 1
+    while count < 10001:
+        num += 2
+        if is_prime(num):
+            count += 1
 
-print num
+    print num
+
+if __name__ == '__main__':
+    main()
