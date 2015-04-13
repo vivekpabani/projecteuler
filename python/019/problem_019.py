155e20c1f510f1b0e3eb2474b7082920d316c975
vivekpabani <viky.pabani@gmail.com>
2015-04-13 12:42:50 -0500

    Def Update

--- python/019/problem_019.py
+++ python/019/problem_019.py
@@ -20,62 +20,62 @@ How many Sundays fell on the first of the month during the twentieth century (1 
 
 """
 
-
 import sys
 
-normal_year = [31,28,31,30,31,30,31,31,30,31,30,31] # days of months for a non leap year
-leap_year =   [31,29,31,30,31,30,31,31,30,31,30,31] # days of months for a leap year
-count_year=   [] 
 
-days, day_first = 0,0
 
-# function to find if given year is leap year or not.
-
-def find_leap(year_input) :
-    if year_input%400 == 0 :
+def find_leap(year_input):
+    """function to find if given year is leap year or not."""
+    if year_input % 400 == 0:
         return 1
-    elif year_input%100 == 0 :
+    elif year_input % 100 == 0:
         return 0
-    elif year_input%4 == 0 :
+    elif year_input % 4 == 0:
         return 1
-    else :
+    else:
         return 0
 
-# function to find the days between year 1900 and given year
 
-
-def count_days(year_input) :
+def count_days(year_input):
+    """function to find the days between year 1900 and given year"""
     total_days = 0
-    for years in range(1900,year_input) :
-        if find_leap(years) :
-            total_days = total_days + 366
-        else :
-            total_days = total_days + 365
+    for years in range(1900, year_input):
+        if find_leap(years):
+            total_days += 366
+        else:
+            total_days += 365
     return total_days
 
 
-ystart = int(sys.argv[1])
-yend = int(sys.argv[2])
-day = int(sys.argv[3])
-flag = 1
+def main():
 
+    normal_year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # days of months for a non leap year
+    leap_year = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # days of months for a leap year
+    days, day_first = 0, 0
+    start_year = int(sys.argv[1])
+    end_year = int(sys.argv[2])
+    day = int(sys.argv[3])
+    flag = 1
 
-if ystart < 1900 or yend < 1900 or ystart > 9999 or yend > 9999 or day < 1 or day > 7 :
-    print "Please provide valid values."
-    flag = 0;
+    if start_year < 1900 or end_year < 1900 or start_year > 9999 or end_year > 9999 or day < 1 or day > 7:
+        print "Please provide valid values."
+        flag = 0
 
-if flag :
-    days = count_days(ystart)
-    
-    for year in range(ystart,yend+1):
-        if find_leap(year) :
-            count_year = leap_year
-        else :
-            count_year = normal_year
-    
-        for months in range(0,12) :
-            if days%7 == (day-1):
-                day_first = day_first + 1
-            days = days + count_year[months]
-        
-    print day_first
+    if flag:
+        days = count_days(start_year)
+
+        for year in range(start_year, end_year + 1):
+            if find_leap(year):
+                count_year = leap_year
+            else:
+                count_year = normal_year
+
+            for months in range(0, 12):
+                if days % 7 == (day - 1):
+                    day_first += 1
+                days = days + count_year[months]
+
+        print day_first
+
+if __name__ == '__main__':
+    main()
